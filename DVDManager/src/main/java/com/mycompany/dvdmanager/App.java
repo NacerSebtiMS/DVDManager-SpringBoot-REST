c634da4548afaafdb93dd98f2b1f1cfd3df35255
Nacer Sebti <nacersebti.ms@gmail.com>
2020-12-16 19:05:49 -0500

    Implemented App

--- DVDManager/src/main/java/com/mycompany/dvdmanager/App.java
+++ DVDManager/src/main/java/com/mycompany/dvdmanager/App.java
@@ -5,10 +5,17 @@
  */
 package com.mycompany.dvdmanager;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  *
  * @author nacer
  */
+
+@SpringBootApplication
 public class App {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
