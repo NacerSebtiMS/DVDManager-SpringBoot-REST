fc31df3732917152c30ec3a27443d835bf4c9909
ReihanehSa <reihanehsadeghi04@gmail.com>
2020-12-17 12:38:49 -0500

    ErrorHandler

--- DVDManager/src/main/java/com/mycompany/dvdmanager/controllers/Error.java
+++ DVDManager/src/main/java/com/mycompany/dvdmanager/controllers/Error.java
@@ -10,5 +10,19 @@ package com.mycompany.dvdmanager.controllers;
  * @author nacer
  */
 public class Error {
-    
+    private String message;
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
